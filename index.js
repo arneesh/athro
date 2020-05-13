1bfea897539333fae12b61f9cdf501578c94c7b6
Arneesh Aima <arneeshaima@Arneeshs-MacBook-Pro.local>
2020-05-13 22:59:53 +0530

    minor

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 export {binarySearch} from './src/searching/binarySearch';
 export {linearSearch} from './src/searching/linearSearch';
 
+
 //==============Sorting==============
 export {bubbleSort} from './src/sorting/bubbleSort';
 export {selectionSort} from './src/sorting/selectionSort';
