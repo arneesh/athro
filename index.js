23ea417d7cfc691cea99228ace8eb0d00c4978c0
Arneesh Aima <arneeshaima@Arneeshs-MacBook-Pro.local>
2020-05-13 17:16:33 +0530

    added generics to index

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
+
+//==============Searching==============
 export {binarySearch} from './src/searching/binarySearch';
 export {linearSearch} from './src/searching/linearSearch';
 
-
+//==============Sorting==============
 export {bubbleSort} from './src/sorting/bubbleSort';
 export {selectionSort} from './src/sorting/selectionSort';
 export {insertionSort} from './src/sorting/insertionSort';
+
+//==============Generics==============
+export {titleCase} from './src/generics/titleCase';
+export {beautifyDate} from './src/generics/beautifyDate';
+export {beautifyNumber} from './src/generics/beautifyNumber';
+export {currencySymbol} from './src/generics/currencySymbol';
+export {handleDecimal} from './src/generics/handleDecimal';
