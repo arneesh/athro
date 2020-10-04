b6e3bda74b8336ca4188f1592d341a057f54091b
arneesh <arneeshaima@gmail.com>
2020-10-04 15:52:42 +0530

    Test

--- test/arrayRemoveDuplicates.test.js
+++ test/arrayRemoveDuplicates.test.js
@@ -1,5 +1,6 @@
 import {arrayRemoveDuplicates}  from '../generics/arrayRemoveDuplicates';
 
+
 test('adds 1 + 2 to equal 3', () => {
   expect(arrayRemoveDuplicates([1,2,4,2,5])).toBe([1,2,4,5]);
 });
