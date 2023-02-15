09083a540be5c7c9bf1db620b9dbe96750571968
arneesh <arneeshaima@gmail.com>
2023-02-15 19:30:36 +0530

    (feat): added breadthfirstsearch

--- src/algorithms/traversal/bfs.ts
+++ src/algorithms/traversal/bfs.ts
@@ -1,1 +1,17 @@
-// add bfs code here
+export const bfs = (root: any) => {
+    if (root === null || root === undefined) return root;
+    var visited = [];
+    var queue = [];
+    var node;
+    queue.push(root);
+
+    while (queue.length > 0) {
+        node = queue.shift();
+        if (node) {
+            visited.push(node.value);
+            if (node.left) queue.push(node.left);
+            if (node.right) queue.push(node.right);
+        }
+    }
+    return visited;
+};
