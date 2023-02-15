b376e8a5728c3c71a08e6da6b2dc2d46d1471101
arneesh <arneeshaima@gmail.com>
2023-02-15 19:41:46 +0530

    (feat): added inorder

--- src/algorithms/traversal/inorder.ts
+++ src/algorithms/traversal/inorder.ts
@@ -9,14 +9,14 @@ class Node<T> {
     }
 }
 
-export function preOrder<T>(root: Node<T>): T[] {
+export function inOrder<T>(root: Node<T>): T[] {
     if (root === null || root === undefined) return root;
-    let visited: T[] = [];
-    let current = root;
+    var visited: T[] = [];
+    var current = root;
 
     function traverse(node: Node<T>) {
-        visited.push(node.value);
         if (node.left) traverse(node.left);
+        visited.push(node.value);
         if (node.right) traverse(node.right);
     }
     traverse(current);
