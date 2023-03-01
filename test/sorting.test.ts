0454cea03cc05aaab1776bd4b5574fb5b48eec0e
arneesh <arneeshaima@gmail.com>
2023-03-02 00:14:09 +0530

    (chore): updated test files, replaced jest with vitest

--- test/sorting.test.ts
+++ test/sorting.test.ts
@@ -1,21 +1,22 @@
+import { it, expect } from 'vitest';
 import { bubbleSort, insertionSort, selectionSort, mergeSort, quickSort } from '../src';
 
-test('sort the array using bubble sort', () => {
+it('sort the array using bubble sort', () => {
     expect(bubbleSort([1, 4, 2, 6, 3])).toEqual(expect.arrayContaining([1, 2, 3, 4, 6]));
 });
 
-test('sort the array using insertion sort', () => {
+it('sort the array using insertion sort', () => {
     expect(insertionSort([1, 4, 2, 6, 3])).toEqual(expect.arrayContaining([1, 2, 3, 4, 6]));
 });
 
-test('sort the array using selection sort', () => {
+it('sort the array using selection sort', () => {
     expect(selectionSort([1, 4, 2, 6, 3])).toEqual(expect.arrayContaining([1, 2, 3, 4, 6]));
 });
 
-test('sort the array using merge sort', () => {
+it('sort the array using merge sort', () => {
     expect(mergeSort([1, 4, 2, 6, 3])).toEqual(expect.arrayContaining([1, 2, 3, 4, 6]));
 });
 
-test('sort the array using merge sort', () => {
+it('sort the array using merge sort', () => {
     expect(quickSort([1, 4, 2, 6, 3])).toEqual(expect.arrayContaining([1, 2, 3, 4, 6]));
 });
